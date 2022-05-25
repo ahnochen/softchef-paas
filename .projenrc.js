ea331dd36d02d6d1d446dce6381f920e8b16736f
ahnochen <ahnochen@softchef.com>
2022-05-25 10:29:44 +0800

    init

--- .projenrc.js
+++ .projenrc.js
@@ -4,9 +4,13 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   defaultReleaseBranch: 'main',
   name: 'softchef-paas',
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    '@softchef/cdk-restapi',
+    'cdk-ecr-deployment',
+  ],
+
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  // packageName: undefined,  /* The 'name' in package.json. */
 });
 project.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,205 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import {
+  App,
+  Stack,
+  StackProps,
+  RemovalPolicy,
+  CfnResource,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as ecspatterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as logs from "aws-cdk-lib/aws-logs";
+import * as servicediscovery from "aws-cdk-lib/aws-servicediscovery";
+import * as iam from "aws-cdk-lib/aws-iam";
+
 
 export class MyStack extends Stack {
+  //Export Vpclink and ALB Listener
+  public readonly httpVpcLink: CfnResource;
+
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    // Create a VPC with 9x subnets divided over 3 AZ's
+    const vpc = new ec2.Vpc(this, "testVpc", {
+      cidr: "172.31.0.0/16",
+      natGateways: 1,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'pbulic',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        }
+      ]
+    });
+
+    // Create an ECS cluster
+    const cluster = new ecs.Cluster(this, "test-cluster", {
+      clusterName: "testCluster",
+      containerInsights: true,
+      vpc: vpc,
+    });
+
+    // Create a Fargate container image
+    const image = ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample");
+
+    // Create higher level construct containing the Fargate service with a load balancer
+    new ecspatterns.ApplicationLoadBalancedFargateService(
+      this,
+      "amazon-ecs-sample",
+      {
+        cluster,
+        circuitBreaker: {
+          rollback: true,
+        },
+        cpu: 256,
+        desiredCount: 1,
+        taskImageOptions: {
+          image,
+          containerPort: 80,
+          logDriver: ecs.LogDrivers.awsLogs({
+            streamPrefix: id,
+            logRetention: logs.RetentionDays.ONE_MONTH,
+          }),
+        },
+      }
+    );
+
+    // Cloud Map Namespace
+    const dnsNamespace = new servicediscovery.PrivateDnsNamespace(
+      this,
+      "DnsNamespace",
+      {
+        name: "http-api.local",
+        vpc: vpc,
+        description: "Private DnsNamespace for Microservices",
+      }
+    );
+
+    // Task Role
+    const taskrole = new iam.Role(this, "ecstestTaskExecutionRole", {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+    });
+    taskrole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AmazonECSTaskExecutionRolePolicy"
+      )
+    );
+
+    // Task Definitions
+    const testServiceTaskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      "testServiceTaskDef",
+      {
+        memoryLimitMiB: 512,
+        cpu: 256,
+        taskRole: taskrole,
+        runtimePlatform: {
+          cpuArchitecture: ecs.CpuArchitecture.ARM64
+        }
+      }
+    );
+
+    // Log Groups
+    const testServiceLogGroup = new logs.LogGroup(this, "testServiceLogGroup", {
+      logGroupName: "/ecs/testService",
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    // Log Driver
+    const testServiceLogDriver = new ecs.AwsLogDriver({
+      logGroup: testServiceLogGroup,
+      streamPrefix: "testService",
+    });
+
+    // Amazon ECR Repositories
+    const testservicerepo = ecr.Repository.fromRepositoryName(
+      this,
+      "author_test-service",
+      "author_test-service"
+    );;
+
+    // Task Containers
+    const testServiceContainer = testServiceTaskDefinition.addContainer(
+      "testServiceContainer",
+      {
+        image: ecs.ContainerImage.fromEcrRepository(testservicerepo),
+        logging: testServiceLogDriver,
+      }
+    );
+
+    testServiceContainer.addPortMappings({
+      containerPort: 80,
+    });
+
+    //Security Groups
+    const testServiceSG = new ec2.SecurityGroup(
+      this,
+      "testServiceSecurityGroup",
+      {
+        allowAllOutbound: true,
+        securityGroupName: "testServiceSecurityGroup",
+        vpc,
+      }
+    );
+
+    testServiceSG.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+
+
+    const service = new ecs.FargateService(this, 'Service', {
+      cluster,
+      taskDefinition: testServiceTaskDefinition,
+      assignPublicIp: false,
+      desiredCount: 0,
+      securityGroups: [testServiceSG],
+      cloudMapOptions: {
+        name: "testService",
+        cloudMapNamespace: dnsNamespace,
+      },
+    });
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', { vpc, internetFacing: true });
+    const listener = lb.addListener('Listener', { port: 80 });
+    service.registerLoadBalancerTargets(
+      {
+        containerName: testServiceContainer.containerName,
+        containerPort: 80,
+        newTargetGroupId: 'ECS',
+        listener: ecs.ListenerConfig.applicationListener(listener, {
+          protocol: elbv2.ApplicationProtocol.HTTP
+        }),
+      },
+    );
+
+    //VPC Link
+    this.httpVpcLink = new CfnResource(this, "HttpVpcLink", {
+      type: "AWS::ApiGatewayV2::VpcLink",
+      properties: {
+        Name: "http-api-vpclink",
+        SubnetIds: vpc.privateSubnets.map((m) => m.subnetId),
+      },
+    });
   }
 }
 
 // for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
+// const devEnv = {
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   region: process.env.CDK_DEFAULT_REGION,
+// };
 
 const app = new App();
 
-new MyStack(app, 'softchef-paas-dev', { env: devEnv });
+new MyStack(app, 'softchef-paas', 
+// { env: devEnv }
+);
 // new MyStack(app, 'softchef-paas-prod', { env: prodEnv });
 
 app.synth();
